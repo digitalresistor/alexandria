770051c8943783be1fb9028641ed952f3382d797
Bert JW Regeer <bertjw@regeer.org>
2014-10-26 19:56:34 -0600

    Import the various models

--- alexandria/models/__init__.py
+++ alexandria/models/__init__.py
@@ -1,1 +1,14 @@
 from .meta import DBSession, Base
+
+from .user import (
+            User,
+            UserTickets,
+        )
+
+from .domain import Domain
+from .record import Record
+
+from .types import (
+        type_to_value,
+        value_to_type,
+        )
