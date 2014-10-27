03f9d65f4653b0b1ca167727bdbbaad43b035ac1
Bert JW Regeer <bertjw@regeer.org>
2014-10-26 18:12:53 -0600

    Fix indentation

--- setup.py
+++ setup.py
@@ -20,29 +20,29 @@ requires = [
         ]
 
 setup(name='alexandria',
-      version='0.0',
-      description='alexandria',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pyramid",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='Bert JW Regeer',
-      author_email='bertjw@regeer.org',
-      url='',
-      keywords='web wsgi bfg pylons pyramid',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      test_suite='alexandria',
-      install_requires=requires,
-      entry_points="""\
-      [paste.app_factory]
-      main = alexandria:main
-      [console_scripts]
-      alexandria_init_db = alexandria.scripts.initializedb:main
-      alexandria_destroy_db = alexandria.scripts.destroydb:main
-      """,
+        version='0.0',
+        description='alexandria',
+        long_description=README + '\n\n' + CHANGES,
+        classifiers=[
+            "Programming Language :: Python",
+            "Framework :: Pyramid",
+            "Topic :: Internet :: WWW/HTTP",
+            "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+            ],
+        author='Bert JW Regeer',
+        author_email='bertjw@regeer.org',
+        url='',
+        keywords='web wsgi bfg pylons pyramid',
+        packages=find_packages(),
+        include_package_data=True,
+        zip_safe=False,
+        test_suite='alexandria',
+        install_requires=requires,
+        entry_points="""\
+        [paste.app_factory]
+        main = alexandria:main
+        [console_scripts]
+        alexandria_init_db = alexandria.scripts.initializedb:main
+        alexandria_destroy_db = alexandria.scripts.destroydb:main
+        """,
       )
