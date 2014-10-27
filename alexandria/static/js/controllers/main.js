991c27ef07c034bc29860ee0963504d32d50eb02
Bert JW Regeer <bertjw@regeer.org>
2014-10-27 00:00:34 -0600

    Add more stuff to the main controller

--- alexandria/static/js/controllers/main.js
+++ alexandria/static/js/controllers/main.js
@@ -1,5 +1,26 @@
-app
-  .controller('MainCtrl', ['$scope',
-    function($scope) {
-    }
-  ]);
+app.controller('MainCtrl', ['$scope', 'User',
+        function($scope, User) {
+            $scope.initMain = function() {
+                $scope.checkLoggedIn();
+            };
+
+            $scope.checkLoggedIn = function() {
+                User.checkLoggedIn().then(function(data) {
+                    console.log("User logged in? %o", data);
+                }).catch(function(data) {
+                    console.error("ERROR! %o", data);
+                });
+            };
+
+            // ============= Watch =============
+            $scope.user = User.getUser();
+            $scope.$on('user.update', function(event) {
+                $scope.user = User.getUser();
+            });
+
+            $scope.isLoggedIn = User.getIsLoggedIn();
+            $scope.$on('user.loggedin.status', function(event) {
+                $scope.isLoggedIn = User.getIsLoggedIn();
+            });
+        }
+]);
