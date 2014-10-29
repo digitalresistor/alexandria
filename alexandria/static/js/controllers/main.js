d1b0a77411e27e8007ce512d7e9aaa557637a63b
Bert JW Regeer <bertjw@regeer.org>
2014-10-29 00:12:44 -0600

    Add the logout() function
    This adds the logout() function to the main controller, so that if you
    click the logout button, it actually does something ;-)

--- alexandria/static/js/controllers/main.js
+++ alexandria/static/js/controllers/main.js
@@ -1,23 +1,33 @@
-app.controller('MainCtrl', ['$scope', '$log', 'User',
-        function($scope, $log, User) {
-            $scope.initMain = function() {
-                $scope.checkLoggedIn();
-            };
+app.controller('MainCtrl', ['$scope', '$route', '$log', 'User',
+    function($scope, $route, $log, User) {
+        $scope.initMain = function() {
+            $scope.checkLoggedIn();
+        };
 
-            $scope.checkLoggedIn = function() {
-                User.checkLoggedIn().then(function(data) {
-                    $log.debug("User logged in: %o", data);
-                }).catch(function(data) {
-                    $log.error("ERROR! %o", data);
-                });
-            };
+        $scope.checkLoggedIn = function() {
+            User.checkLoggedIn().then(function(data) {
+                $log.debug("User logged in: %o", data);
+            }).catch(function(data) {
+                $log.error("Checking to see if we are logged in failed, hard!");
+            });
+        };
 
-            // ============= Watch =============
+        $scope.logout = function(event) {
+            User.logout().then(function(data) {
+                $log.debug('User has been logged out...');
+            }).catch(function(data) {
+                $log.debug('Unable to log user out...');
+            });
+
+        }
+
+        // ============= Watch =============
+        $scope.user = User.getUser();
+        $scope.isLoggedIn = User.getIsLoggedIn();
+        $scope.$on('user', function(event) {
+            $log.debug("User changed: {%o, %o}", User.getUser(), User.getIsLoggedIn());
             $scope.user = User.getUser();
             $scope.isLoggedIn = User.getIsLoggedIn();
-            $scope.$on('user', function(event) {
-                $scope.user = User.getUser();
-                $scope.isLoggedIn = User.getIsLoggedIn();
-            });
-        }
+        });
+    }
 ]);
