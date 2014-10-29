22a784c48a4ab8f0490d5272d76f73d5daa4d2d2
Bert JW Regeer <bertjw@regeer.org>
2014-10-29 00:10:10 -0600

    Add the login controller

---
+++ alexandria/static/js/controllers/login.js
@@ -0,0 +1,14 @@
+app.controller('LoginCtrl', ['$scope', '$log', '$route', 'User',
+    function($scope, $log, $route, User) {
+        $scope.loginForm = {};
+
+        $scope.login = function() {
+            User.login($scope.loginForm.username, $scope.loginForm.password).then(function(data) {
+                $route.reload();
+            }).catch(function(data) {
+                $log.debug('Unable to log user in...');
+            });
+        };
+    }
+]);
+
