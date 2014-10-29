a020c9861045e8d4163e717092b2a3232a90b9e1
Bert JW Regeer <bertjw@regeer.org>
2014-10-28 21:34:14 -0600

    The CSRF cookie name is different
    Because I am too lazy to go add a new predicate to the Pyramid app, I am
    just going to change them in javascript.

--- alexandria/static/js/app.js
+++ alexandria/static/js/app.js
@@ -2,3 +2,8 @@ var app = angular.module('app', [
         'ngResource',
         'ngRoute'
         ]);
+
+app.config(['$httpProvider', function($httpProvider) {
+    $httpProvider.defaults.xsrfHeaderName = 'X-CSRF-Token';
+    $httpProvider.defaults.xsrfCookieName = 'CSRF-Token';
+}]);
