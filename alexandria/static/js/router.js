f4277395b4906b9819c50e346d7b9f0f2e954f36
Bert JW Regeer <bertjw@regeer.org>
2014-10-29 00:05:54 -0600

    When route changes, check if user is logged in.
    If the user is not logged in, we actually change the template/controller
    over to the Login controller, this forces the user to immediately log
    int to the application.

--- alexandria/static/js/router.js
+++ alexandria/static/js/router.js
@@ -16,6 +16,20 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
     });
 }]);
 
+app.run(['$rootScope', '$log', 'User', function ($rootScope, $log, User) {
+    $rootScope.$on('$routeChangeStart', function(event, next, current) {
+
+        // Check to see if the user is logged in ...
+        if (!User.getIsLoggedIn()) {
+            $log.debug('User is not logged in... changing html.');
+            next.templateUrl = '/html/login.html';
+            next.controller = 'LoginCtrl';
+        } else {
+            $log.debug('User is logged in. Continuing on.');
+        }
+    });
+}]);
+
 app.run(['$rootScope', '$log', '$route', 'User', function ($rootScope, $log, $route, User) {
     $rootScope.$on('user', function(event) {
         $route.reload();
