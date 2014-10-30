7b7676ffc3e98d9e86e242ad31546c72c8cbed22
Bert JW Regeer <bertjw@regeer.org>
2014-10-29 23:41:30 -0600

    Add angular directive "server-error"

--- alexandria/static/js/app.js
+++ alexandria/static/js/app.js
@@ -7,3 +7,20 @@ app.config(['$httpProvider', function($httpProvider) {
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRF-Token';
     $httpProvider.defaults.xsrfCookieName = 'CSRF-Token';
 }]);
+
+app.directive('serverError', function() {
+    return {
+        restrict: 'A',
+        require: '?ngModel',
+        link: function(scope, element, attrs, ctrl) {
+            element.on('change', function() {
+                scope.$apply(
+                    function() {
+                        ctrl.$setValidity('server', true);
+                    })
+                }
+            );
+        }
+    };
+});
+
