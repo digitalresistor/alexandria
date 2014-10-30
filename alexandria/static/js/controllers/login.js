1eedfeceaf37751f89f3a947d24251faf53a2673
Bert JW Regeer <bertjw@regeer.org>
2014-10-29 23:45:00 -0600

    The form now has error messaging
    When the server returns an error, we can display it on the form!

--- alexandria/static/js/controllers/login.js
+++ alexandria/static/js/controllers/login.js
@@ -1,12 +1,19 @@
 app.controller('LoginCtrl', ['$scope', '$log', '$route', 'User',
     function($scope, $log, $route, User) {
         $scope.loginForm = {};
+        $scope.errors = {};
 
         $scope.login = function() {
+            $log.debug('Attempting to log user in...');
             User.login($scope.loginForm.email, $scope.loginForm.password).then(function(data) {
                 $route.reload();
             }).catch(function(data) {
-                $log.debug('Unable to log user in...');
+                $log.debug('Unable to log user in... %o', data);
+
+                angular.forEach(data.errors, function(error, field) {
+                    $scope.form[field].$setValidity('server', false);
+                    $scope.errors[field] = error;
+                });
             });
         };
     }
