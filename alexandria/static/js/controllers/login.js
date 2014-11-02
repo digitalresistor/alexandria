05bbbdb1c80756600a6ad31bf17eeb842ee58edd
Bert JW Regeer <bertjw@regeer.org>
2014-11-01 23:05:30 -0600

    Set the form elements to $dirty

--- alexandria/static/js/controllers/login.js
+++ alexandria/static/js/controllers/login.js
@@ -4,6 +4,14 @@ app.controller('LoginCtrl', ['$scope', '$log', '$route', 'User',
         $scope.errors = {};
 
         $scope.login = function() {
+            $log.debug('Setting all the form fields to $dirty...')
+
+            angular.forEach($scope.form, function(ctrl, field) {
+                if (typeof ctrl === 'object' && ctrl.hasOwnProperty('$modelValue')) {
+                    ctrl.$dirty = true;
+                }
+            });
+
             $log.debug('Attempting to log user in...');
             User.login($scope.loginForm.email, $scope.loginForm.password).then(function(data) {
                 $route.reload();
