98c207ea262e500ea4f5c338a9bb5642047b24b7
Bert JW Regeer <bertjw@regeer.org>
2014-11-02 19:20:13 -0700

    Change the timeout, and reissue_time
    We want to make sure that the session never expires, since this also
    causes our CSRF token to expire, which causes issues...

--- alexandria/session.py
+++ alexandria/session.py
@@ -5,7 +5,9 @@ def includeme(config):
     _session_factory = SignedCookieSessionFactory(
             config.registry.settings['pyramid.secret.session'],
             httponly=True,
-            max_age=864000
+            max_age=864000,
+            timeout=864000,
+            reissue_time=1200,
             )
 
     config.set_session_factory(_session_factory)
