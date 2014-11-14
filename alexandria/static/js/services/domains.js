5cae1fcb7ee151df2513ea7af1bf1b6929b81491
Bert JW Regeer <bertjw@regeer.org>
2014-11-13 23:40:52 -0700

    We want to cache the resultset

--- alexandria/static/js/services/domains.js
+++ alexandria/static/js/services/domains.js
@@ -1,7 +1,21 @@
-app.factory('Domains', ['$resource', function($resource) {
+app.factory('DomainsFactory', ['$resource', function($resource) {
     return $resource('/domain/:id', { id: '@id' }, {
         update: {
             method: 'PUT'
         }
     });
 }]);
+
+app.service('Domains', ['$rootScope', '$log', '$q', 'DomainsFactory',
+    function($rootScope, $log, $q, DomainsFactory) {
+        var service = {};
+
+        service.query = function(ret_func) {
+                service.domains = DomainsFactory.query(ret_func);
+                return service.domains;
+            }
+        };
+
+        return service;
+    }
+]);
