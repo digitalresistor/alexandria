ba4344d37315290cfac68e19a39ffb3accd579a7
Bert JW Regeer <bertjw@regeer.org>
2014-11-14 00:28:25 -0700

    We want to cache the domain information

--- alexandria/static/js/services/domains.js
+++ alexandria/static/js/services/domains.js
@@ -10,9 +10,17 @@ app.service('Domains', ['$rootScope', '$log', '$q', 'DomainsFactory',
     function($rootScope, $log, $q, DomainsFactory) {
         var service = {};
 
+        service.all = [];
         service.query = function(ret_func) {
-                service.domains = DomainsFactory.query(ret_func);
-                return service.domains;
+            if (service.all.length == 0) {
+                $log.debug("Fetching list of domains.");
+                service.all = DomainsFactory.query(ret_func);
+
+                return service.all;
+            }
+            else {
+                ret_func();
+                return service.all;
             }
         };
 
